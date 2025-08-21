ef8433304e289553fc743cb873ddc3383dfa01fe
ItzCraft <tundukartem2397@gmail.com>
2025-08-21 10:44:52 +0300

    pls

--- src/hjsonpp/expand/TileGenerator.java
+++ src/hjsonpp/expand/TileGenerator.java
@@ -1,9 +1,11 @@
 package hjsonpp.expand;
 
 import arc.struct.Seq;
+import arc.util.Scaling;
 import mindustry.game.Team;
+import mindustry.ui.Styles;
 import mindustry.world.*;
-import mindustry.world.meta.Stat;
+import mindustry.world.meta.*;
 
 public class TileGenerator extends AdvancedConsumeGenerator{
     public Seq<Block> filter = new Seq<>();
@@ -14,10 +16,24 @@ public class TileGenerator extends AdvancedConsumeGenerator{
     @Override
     public void setStats(){
         super.setStats();
-        stats.add(Stat.output, "@x@ ~ @x@");
+        stats.add(Stat.tiles, table->{
+            table.row();
+
+            for(var plan : filter){
+                table.table(Styles.grayPanel, t -> {
+                    t.image(plan.uiIcon).scaling(Scaling.fit).size(40).pad(10f).left().with(i -> StatValues.withTooltip(i, plan));
+                    t.table(info -> {
+                        info.defaults().left();
+                        info.add(plan.localizedName);
+                        info.row();
+                    }).left();
+                });
+            }
+        });
     }
 
-    public boolean canPlaceOn(Tile tile, Team team){
+    @Override
+    public boolean canPlaceOn(Tile tile, Team team, int rotation){
         for(Block floor : filter){
             if(tile.floor() == floor.asFloor()){
                 return true;
