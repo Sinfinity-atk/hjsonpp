94144038335a82f46b88d1ffd52df32ef1d9357e
ItzCraft <tundukartem2397@gmail.com>
2025-08-21 20:36:25 +0300

    ColiderCrafter

---
+++ src/hjsonpp/expand/ColiderCrafter.java
@@ -0,0 +1,57 @@
+package hjsonpp.expand;
+
+import arc.math.Mathf;
+import arc.util.Time;
+import hjsonpp.expand.meta.AdditionalStats;
+import mindustry.world.blocks.production.GenericCrafter;
+
+public class ColiderCrafter extends GenericCrafter{
+    // what chance it to produce item/liquid. 1 = 100%
+    public double produceChance = 0.5f;
+
+    public ColiderCrafter(String name){
+        super(name);
+    }
+
+    @Override
+    public void setStats(){
+        stats.add(AdditionalStats.produceChance, produceChance * 100f + "%");
+        super.setStats();
+    }
+
+    public boolean chance(double produceChance){
+        return Mathf.chance(produceChance);
+    }
+
+    public class ColiderCrafterBuild extends  GenericCrafterBuild{
+        @Override
+        public void updateTile(){
+            if(efficiency > 0){
+
+                progress += getProgressIncrease(craftTime);
+                warmup = Mathf.approachDelta(warmup, warmupTarget(), warmupSpeed);
+
+                if(outputLiquids != null){
+                    float inc = getProgressIncrease(1f);
+                    for(var output : outputLiquids){
+                        handleLiquid(this, output.liquid, Math.min(output.amount * inc, liquidCapacity - liquids.get(output.liquid)));
+                    }
+                }
+
+                if(wasVisible && Mathf.chanceDelta(updateEffectChance)){
+                    updateEffect.at(x + Mathf.range(size * updateEffectSpread), y + Mathf.range(size * updateEffectSpread));
+                }
+            }else{
+                warmup = Mathf.approachDelta(warmup, 0f, warmupSpeed);
+            }
+            totalProgress += warmup * Time.delta;
+
+            if(progress >= 1f){
+                boolean chanced = Mathf.chance(produceChance);
+                if(chanced)craft();
+            }
+
+            dumpOutputs();
+        }
+    }
+}
---
+++ src/hjsonpp/expand/meta/AdditionalStats.java
@@ -0,0 +1,9 @@
+package hjsonpp.expand.meta;
+
+import mindustry.world.meta.Stat;
+import mindustry.world.meta.StatCat;
+
+public class AdditionalStats{
+    public static Stat
+    produceChance = new Stat("produce-chance", StatCat.crafting);
+}
