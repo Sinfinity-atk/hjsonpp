39a06c0ead91e946d7dc2704c07fd457fdcd37cb
ItzCraft <tundukartem2397@gmail.com>
2025-09-19 10:43:43 +0300

    minor fixes

--- src/hjsonpp/HjsonPlusPlusMod.java
+++ src/hjsonpp/HjsonPlusPlusMod.java
@@ -10,12 +10,14 @@ public class HjsonPlusPlusMod extends Mod{
         ClassMap.classes.put("TileGenerator", hjsonpp.expand.TileGenerator.class);
         ClassMap.classes.put("AdvancedCoreBlock", hjsonpp.expand.AdvancedCoreBlock.class);
         ClassMap.classes.put("GeneratorCoreBlock", hjsonpp.expand.GeneratorCoreBlock.class);
+        ClassMap.classes.put("ShieldCoreBlock", hjsonpp.expand.ShieldCoreBlock.class);
         ClassMap.classes.put("ColliderCrafter", hjsonpp.expand.ColiderCrafter.class);
         ClassMap.classes.put("AccelTurret", hjsonpp.expand.AccelTurret.class);
         ClassMap.classes.put("OverHeatTurret", hjsonpp.expand.OverHeatTurret.class);
         ClassMap.classes.put("RestorableWall", hjsonpp.expand.RestorableWall.class);
         ClassMap.classes.put("AdjustableShieldWall", hjsonpp.expand.AdjustableShieldWall.class);
         ClassMap.classes.put("AdjustableBeamNode", hjsonpp.expand.AdjustableBeamNode.class);
+        ClassMap.classes.put("DrawableBlock", hjsonpp.expand.DrawableBlock.class);
         ClassMap.classes.put("TiledFloor", hjsonpp.expand.TiledFloor.class);
         ClassMap.classes.put("DrawTeam", hjsonpp.expand.DrawTeam.class);
         ClassMap.classes.put("EffectWeapon", hjsonpp.expand.EffectWeapon.class);
