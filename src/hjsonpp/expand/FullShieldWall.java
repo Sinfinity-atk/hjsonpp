f659bacb0273ec7504aaa5f83bea8b5bc68b4b0c
Sinfinity-atk <arriquewingsofchaos@gmail.com>
2025-09-30 23:52:21 +0600

    Update FullShieldWall.java

--- src/hjsonpp/expand/FullShieldWall.java
+++ src/hjsonpp/expand/FullShieldWall.java
@@ -97,7 +97,7 @@ public class FullShieldWall extends Wall {
                     });
                 } else if ("block".equalsIgnoreCase(blockUnitsFrom)) {
                     // block footprint-based blocking
-                    Units.nearbyEnemies(team, tile.drawx(), tile.drawy(), block.size * 8f, block.size * 8f, (Unit unit) -> {
+                    Units.nearbyEnemies(team, tile.drawx(), tile.drawy(), block.size * 16f, block.size * 16f, (Unit unit) -> {
                         if (unit.within(x, y, block.size * 16f)) {
                             unit.vel.setZero();
                             unit.move(Tmp.v1.set(unit).sub(this).setLength(1f)); // push slightly out
